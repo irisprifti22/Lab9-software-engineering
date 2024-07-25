47a83b866dca6f7e19f8703b8a32673824aa96bc
IrisPrifti <15013447@sfcollege.edu>
2024-07-25 01:19:39 -0400

    started lab 9 functions

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Lab 9
 
+
+#Takes a string of numbers, and returns a string of the numbers after adding 3
+def encode(password):
+    # my implementation
+    pass
+
+
+def decode(password):
+    # partner implementation
+    pass
