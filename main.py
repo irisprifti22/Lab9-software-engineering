768da96c5944a9d0e1aec4a7769cb5433b4722d3
IrisPrifti <15013447@sfcollege.edu>
2024-07-25 01:38:22 -0400

    changed comments to reflect lab 9 instructions

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-# Lab 9
+# Lab 9: create a password encoder/decoder program with a partner
 
-
-#Takes a string of numbers, and returns a string of the numbers after adding 3
+# takes a string of numbers, and returns a string of the numbers after adding 3
 def encode(password):
     # my implementation
     pass
 
 
+# takes a string of numbers, and returns a string of the numbers after subtracting 3
 def decode(password):
     # partner implementation
     pass
+
+
+# main function: encode, decode, and exit options
