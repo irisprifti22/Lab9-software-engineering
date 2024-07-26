087fce27806f33679dbedbba8d4f49c5c03b44a2
IrisPrifti <15013447@sfcollege.edu>
2024-07-26 11:13:51 -0400

    completed the main function with menu options, added comments

--- main.py
+++ main.py
@@ -15,7 +15,33 @@ def decode(password):
     decoded_password = ''.join(str((int(char) - 3) % 10) for char in password)
     return decoded_password
 
-# main function: encode, decode, and exit options
-if __name__ == '__main__':
+# main function implementation
+def main():
+    stored_password = ''
     while True:
-        break
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        # menu options
+        if option == '1':
+            password = input("Please enter the password to encode: ")
+            stored_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == '2':
+            if stored_password:
+                original_password = decode(stored_password)
+                print(f"The encoded password is {stored_password}, and the original password is {original_password}.")
+            else:
+                print("No password has been encoded yet.")
+        elif option == '3':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+# run the main function
+main()
